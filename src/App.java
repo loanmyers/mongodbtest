cb9624cdcb04d6cc6860a16b87ffdb63bc433266
Loan Myers <loan.myers@gmail.com>
2014-04-26 16:59:41 +1000

    testing mongodb

--- src/App.java
+++ src/App.java
@@ -1,3 +1,8 @@
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import java.net.UnknownHostException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: loanmyers
@@ -5,5 +10,22 @@
  * Time: 4:27 PM
  * To change this template use File | Settings | File Templates.
  */
+
 public class App {
+
+    public static void main(String[] args) {
+        try{
+            MongoClient client = new MongoClient("localhost");
+            DB db = client.getDB("books");
+            DBCollection collection = db.getCollection("books");
+
+            System.out.print(collection);
+
+        } catch(UnknownHostException e){
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
